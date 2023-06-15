d335397048e50fed23c1b5ea3053fad3ed79cd03
Jayasatwik123 <132732183+Jayasatwik123@users.noreply.github.com>
2023-06-15 23:57:27 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+const url="https://tourreservation.onrender.com/api";
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
